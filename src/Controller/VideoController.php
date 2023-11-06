a6f04aecf66f4dbb9663164d2152c9bb1485a32e
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-06 11:53:02 +0100

    .

---
+++ src/Controller/VideoController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/VideoController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VideoController extends AbstractController
+{
+    /**
+     * @Route("/video", name="video")
+     */
+    public function showVideo(): Response
+    {
+        return $this->render('video/show.html.twig');
+    }
+}
+
