ad871da7ea3ff235adc275561e95b18b4bc9b9c2
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-09 16:09:10 +0100

    .

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,14 +5,30 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategorieRepository;
+use App\Repository\PlatRepository;
 
 class AccueilController extends AbstractController
 {
+    private $categorieRepository;
+    private $platRepository;
+
+    public function __construct(CategorieRepository $categorieRepository, PlatRepository $platRepository)
+    {
+        $this->categorieRepository = $categorieRepository;
+        $this->platRepository = $platRepository;
+    }
+
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
+         // Utilise les repositories pour récupérer les données
+         $plats = $this->platRepository->findAll();
+         $categories = $this->categorieRepository->findAll();
+
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'plats' => $plats,
+            'categories' => $categories,
         ]);
     }
     
