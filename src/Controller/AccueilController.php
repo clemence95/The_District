fc12f184543a5a6cfebd01efd52597176d2e6602
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-10 11:53:17 +0100

    .

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,14 +4,23 @@
 
 namespace App\Controller;
 
+use App\Repository\CategorieRepository;
+use App\Repository\PlatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
+    private $platRepository;
+    private $categorieRepo;
+    private $em;
+
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function __construct(PlatRepository $platRepo, CategorieRepository $categorieRepo)
+    {
+        
+    }
     {
         $entityManager = $this->getDoctrine()->getManager();
         $platRepository = $entityManager->getRepository('App\Entity\Plat');
