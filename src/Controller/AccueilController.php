34b6f396231ce50a0bb59f6af23f4943b3403727
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-10 11:11:18 +0100

    .

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -7,22 +7,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Service\AccueilService;
 
 class AccueilController extends AbstractController
 {
-    private AccueilService $accueilService;
-
-    public function __construct(AccueilService $accueilService)
-    {
-        $this->accueilService = $accueilService;
-    }
-
     #[Route('/accueil', name: 'app_accueil')]
     public function index(): Response
     {
-        $categories = $this->accueilService->getAllCategories('NomCategorie');
-        $plats = $this->accueilService->getAllPlats();  // Utilisez la méthode pour récupérer tous les plats
+        $entityManager = $this->getDoctrine()->getManager();
+        $platRepository = $entityManager->getRepository('App\Entity\Plat');
+        
+        $categories = $platRepository->getAllCategories('NomCategorie');
+        $plats = $platRepository->getAllPlats();
 
         // ...
 
