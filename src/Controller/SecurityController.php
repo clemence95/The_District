961e497d96043add15ce41c1d485407926a76e97
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-14 16:50:18 +0100

    .

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,5 +1,7 @@
 <?php
 
+// src/Controller/SecurityController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,11 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/security', name: 'app_security')]
-    public function index(): Response
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(): Response
     {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+        // Logique de connexion ici
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        // Cette méthode ne sera jamais exécutée.
+        throw new \Exception('Ne devrait jamais être atteint');
     }
 }
+
