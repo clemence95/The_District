bd4cecf044c8dfe6290b99d5db07b69adf476c58
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-27 11:28:52 +0100

    .

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -1,18 +1,66 @@
 <?php
 
+// src/Controller/CommandeController.php
+
 namespace App\Controller;
 
+use App\Repository\PlatsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Commande;
+use App\Entity\Detail;
 
 class CommandeController extends AbstractController
 {
-    #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
+    #[Route('/commande', name: 'commande')]
+    public function passerCommande(Request $request, PlatsRepository $platsRepository, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('commande/index.html.twig', [
-            'controller_name' => 'CommandeController',
-        ]);
+        // Récupère les éléments du panier depuis le service PanierService
+        $panier = []; // Remplacez par la logique de récupération du panier
+
+        $dateCommande = new \DateTime();
+        $montantTotalPanier = 0;
+
+        $nouvelleCommande = new Commande();
+
+        foreach ($panier as $element) {
+            $plat = $platsRepository->find($element['plat_id']);
+
+            $montantTotalPanier += $plat->getPrix() * $element['quantity'];
+
+            $detail = new Detail();
+            $detail->setPlat($plat);
+            $detail->setQuantite($element['quantity']);
+
+            $nouvelleCommande->addDetail($detail);
+        }
+
+        $nouvelleCommande->setDateCommande($dateCommande);
+        $nouvelleCommande->setTotal($montantTotalPanier);
+        $nouvelleCommande->setEtat('En attente');
+
+        $utilisateur = $this->getUser();
+        if ($utilisateur) {
+            $nouvelleCommande->setUtilisateur($utilisateur);
+        }
+
+        $entityManager->persist($nouvelleCommande);
+        $entityManager->flush();
+
+        // Redirige vers une page de confirmation ou une autre page appropriée
+        return $this->redirectToRoute('confirmation_commande', ['id' => $nouvelleCommande->getId()]);
+    }
+
+    #[Route('/confirmation-commande/{id}', name: 'confirmation_commande')]
+    public function confirmationCommande(Commande $commande): Response
+    {
+        // Affiche la page de confirmation avec les détails de la commande
+        return $this->render('commande/confirmation.html.twig', ['commande' => $commande]);
     }
 }
+
+
+
--- src/Form/CommandeType.php
+++ src/Form/CommandeType.php
@@ -1,24 +1,29 @@
 <?php
 
+// src/Form/CommandeType.php
+
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommandeType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
-        ;
+            ->add('adresseLivraison', TextType::class, ['label' => 'Adresse de livraison'])
+            ->add('adresseFacturation', TextType::class, ['label' => 'Adresse de facturation'])
+            ->add('modePaiement', TextType::class, ['label' => 'Mode de paiement']);
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            // Configurez ici vos options de formulaire, si nécessaire
         ]);
     }
 }
+
