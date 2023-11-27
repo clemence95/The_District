7ed56553dbc0ac8e69b7c9ca87ee9c2fed6f8152
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-27 14:04:09 +0100

    .

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -3,23 +3,31 @@
 
 namespace App\Controller;
 
-use App\Service\PanierService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use App\Service\PanierService;
+use App\Form\CommandeType;
 
 class CommandeController extends AbstractController
 {
-    #[Route('/commande/afficher-formulaire', name: 'commande_afficher_formulaire')]
-    public function afficherFormulaire(PanierService $panierService): Response
+    #[Route('/valider-commande', name: 'commande_valider', methods: ['POST'])]
+    public function validerCommande(Request $request, PanierService $panierService): Response
     {
-        // Récupérez le panier depuis votre service PanierService
-        $panier = $panierService->getPanier();
+        // Créez une instance du formulaire
+        $form = $this->createForm(CommandeType::class);
 
-        return $this->render('commande/index.html.twig', ['panier' => $panier]);
+        // Traitez le formulaire s'il a été soumis
+        $form->handleRequest($request);
+
+        // Votre logique pour valider la commande
+
+        return $this->render('commande/index.html.twig', [
+            'panier' => $panierService->getPanier(),
+            'form' => $form->createView(), // Transmettez le formulaire au modèle
+        ]);
     }
-
     // ... autres actions de votre contrôleur
 }
 
