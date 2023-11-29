c1d5ad0cfe50e7b047addfcba9c157697d0dde10
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-11-29 16:17:06 +0100

    .

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -1,5 +1,4 @@
 <?php
-
 // src/Controller/CommandeController.php
 
 namespace App\Controller;
@@ -7,61 +6,88 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Commande;
+use App\Service\PanierService;
+use App\Form\CommandeType;
 use App\Entity\Detail;
-use App\Repository\PlatRepository; // Assurez-vous que cette ligne est présente
+use App\Entity\Commande;
+use Doctrine\ORM\EntityManagerInterface;
+
+// src/Controller/CommandeController.php
+
 
 class CommandeController extends AbstractController
 {
-    #[Route('/valider-commande', name: 'valider_commande', methods: ['POST'])]
-    public function validerCommande(Request $request, PlatRepository $platRepository, EntityManagerInterface $entityManager): Response
+    #[Route('/valider-commande', name: 'commande_valider', methods: ['POST'])]
+    public function validerCommande(Request $request, PanierService $panierService, EntityManagerInterface $entityManager): Response
     {
-        // Obtenez les éléments du panier depuis votre logique existante
-        $panier = []; 
+        // Créez une instance du formulaire
+        $form = $this->createForm(CommandeType::class);
 
-        // Initialisez le montant total du panier
-        $montantTotalPanier = 0;
+        // Traitez le formulaire s'il a été soumis
+        $form->handleRequest($request);
 
-        // Créez une nouvelle instance de la classe Commande
-        $nouvelleCommande = new Commande();
+        // Récupérez le panier
+        $panier = $panierService->getPanier();
 
-        // Parcourez chaque élément du panier
-        foreach ($panier as $element) {
-            // Récupérez le plat associé à l'élément du panier depuis le repository
-            $plat = $platRepository->find($element['plat_id']);
+        // Calculez le total du prix des plats dans le panier
+        $totalPrixPlats = 0;
+        foreach ($panier as $panierItem) {
+            // Assurez-vous que les clés 'plat' et 'quantite' existent dans $panierItem
+            if (isset($panierItem['plat'], $panierItem['quantite'])) {
+                $prix = floatval($panierItem['plat']['prix']); // ou (float)$panierItem['plat']['prix']
+                $quantite = $panierItem['quantite'];
+                $totalPrixPlats += $prix * $quantite;
+            } else {
+                // Gérez le cas où une clé est manquante
+            }
+        }
+        
+        // Votre logique pour valider la commande
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Créez une nouvelle commande
+            $commande = new Commande();
+            $commande->setDateCommande(new \DateTime());
+            $commande->setTotal($totalPrixPlats);
+            $commande->setEtat(0); // Mettez l'état initial de la commande
+            $commande->setUtilisateur($this->getUser()); // Assurez-vous que vous avez un utilisateur connecté
 
-            // Ajoutez le montant du plat multiplié par la quantité au total du panier
-            $montantTotalPanier += $plat->getPrix() * $element['quantity'];
+            $entityManager->persist($commande);
+            $entityManager->flush();
 
-            // Créez une nouvelle instance de la classe Detail
-            $detail = new Detail();
+            // Utilisez l'ID de la commande pour lier les détails de la commande
+            foreach ($panier as $panierItem) {
+                // Assurez-vous que les clés 'plat' et 'quantite' existent dans $panierItem
+                if (isset($panierItem['plat'], $panierItem['quantite'])) {
+                    $detailsCommande = new Detail();
+                    $detailsCommande->setCommande($commande);
+                    $detailsCommande->setPlat($panierItem['plat']);
+                    $detailsCommande->setQuantite($panierItem['quantite']);
+                    // ... autres propriétés des détails de la commande
 
-            // Configurez les propriétés du détail (relation entre Commande et Plats)
-            $detail->setPlat($plat);
-            $detail->setQuantite($element['quantity']);
+                    $entityManager->persist($detailsCommande);
+                } else {
+                    // Gérez le cas où une clé est manquante
+                    // Log ou affichez un message d'erreur selon votre besoin
+                    // Vous pouvez également ajouter une redirection vers une page d'erreur
+                }
+            }
+            
+            $entityManager->flush();
 
-            // Ajoutez le détail à la commande
-            $nouvelleCommande->addDetail($detail);
+            // Effacez le panier après la validation de la commande
+            $panierService->viderPanier();
+
+            // Redirigez l'utilisateur vers une page de confirmation ou une autre page appropriée
+            return $this->redirectToRoute('app_home');
         }
 
-        // Configurez les propriétés de la commande
-        $nouvelleCommande->setDateCommande(new \DateTime());
-        $nouvelleCommande->setTotal($montantTotalPanier);
-        $nouvelleCommande->setEtat(Commande::ETAT_EN_COURS_PREPARATION);
-
-        $utilisateur = $this->getUser();
-        if ($utilisateur) {
-            $nouvelleCommande->setUtilisateur($utilisateur);
-        }
-
-        // Persistez la nouvelle commande dans la base de données
-        $entityManager->persist($nouvelleCommande);
-        $entityManager->flush();
-
-        // Redirigez vers la page de succès
-        return $this->redirectToRoute('app_home');
+        return $this->render('commande/index.html.twig', [
+            'panier' => $panier,
+            'form' => $form->createView(),
+            'totalPrixPlats' => $totalPrixPlats,
+        ]);
     }
-}
 
+    // ... autres actions de votre contrôleur
+}
